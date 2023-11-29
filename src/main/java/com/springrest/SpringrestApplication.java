5606905e2236561e4eb29fdba682c630026f09eb
RaviDobariya <124256250+rajvirDobariya@users.noreply.github.com>
2023-11-29 18:41:57 +0530

    changes for calling apis from react js

--- src/main/java/com/springrest/SpringrestApplication.java
+++ src/main/java/com/springrest/SpringrestApplication.java
@@ -2,8 +2,10 @@ package com.springrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin
 public class SpringrestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springrest/controller/CourseController.java
+++ src/main/java/com/springrest/controller/CourseController.java
@@ -3,14 +3,7 @@ package com.springrest.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.springrest.dto.CourseDTO;
 import com.springrest.service.CourseService;
@@ -18,6 +11,7 @@ import com.springrest.service.CourseService;
 
 @RestController
 @RequestMapping("/courses")
+@CrossOrigin
 public class CourseController {
 
     @Autowired
